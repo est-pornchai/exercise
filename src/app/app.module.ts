640855a1969e1ccb7bd586b0819537eaebbfb519
est-pornchai <pornchai.tdcr@gmail.com>
2020-08-19 15:59:39 +0700

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AgGridModule } from 'ag-grid-angular';
-//import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { Routes, RouterModule } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { FormsModule } from '@angular/forms';
@@ -11,7 +11,7 @@ import { FormsModule } from '@angular/forms';
   
 const routes: Routes = [
 
-//{path: 'formupdate', component: HeroDetailComponent},
+{path: 'test', component: HeroDetailComponent},
 {path: 'user', component: UserComponent},
 {path: 'home', component: AppComponent}
 
@@ -19,7 +19,7 @@ const routes: Routes = [
 
 
 @NgModule({
-  declarations: [AppComponent,  UserComponent],
+  declarations: [AppComponent, HeroDetailComponent, UserComponent],
   imports: [
     BrowserModule,
     AgGridModule.withComponents([]),
---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular';
+import { ViewChild ,Input} from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.scss']
+})
+
+
+export class HeroDetailComponent  {
+}
